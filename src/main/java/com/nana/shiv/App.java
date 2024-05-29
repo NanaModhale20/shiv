578728b2b090a27d1f30dbc3dc67abde4199f3c7
NanaModhale20 <nana.modhale@nciportal.com>
2024-05-29 13:49:18 +0530

    Update App.java

--- src/main/java/com/nana/shiv/App.java
+++ src/main/java/com/nana/shiv/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("checking pull  (fetcch and merge)");
+        System.out.println("mm");
     }
 }
